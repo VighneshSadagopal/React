1f7b37c141bc31329d37e09a650c9d6f6ae7488d
Vighnesh <vighnesh@pop-os.localdomain>
2022-08-17 17:35:41 +0530

    Added 2 exercise second and third

---
+++ ex 2 tictactoe/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Board from "./Components/Board";
+function App() {
+  return (
+    <div className="App App-header">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
